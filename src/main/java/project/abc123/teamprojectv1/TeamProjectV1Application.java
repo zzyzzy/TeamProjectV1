3c1884952e3d4ef8e9da9460ec9cb62154288f2c
zzyzzy <zzyzigy@gmail.com>
2025-02-27 14:44:10 +0900

    Initial commit

---
+++ src/main/java/project/abc123/teamprojectv1/TeamProjectV1Application.java
@@ -0,0 +1,13 @@
+package project.abc123.teamprojectv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProjectV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/project/abc123/teamprojectv1/TeamProjectV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package project.abc123.teamprojectv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
